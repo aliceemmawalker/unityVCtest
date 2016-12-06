74713cfc3e14858d68bcd82d8134e10266aac3b2
Alice Emma Walker <walker.alice.e@gmail.com>
2016-12-06 12:49:26 +0800

    Add files

---
+++ Assets/miniMap.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+//var target: Transform;
+
+function Start () {
+
+}
+
+function Update () {
+//gameObject.transform.position.x = target.transform.position.x;
+//gameObject.transform.position.z = target.transform.position.z;
+}
